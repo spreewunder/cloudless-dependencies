ab747f46f4f96ab12a0d5dded22684a6aa48aab9
Michel Neumann <neumann.michel93@gmail.com>
2018-07-11 14:27:30 +0200

    Update cloudless-dependencies to v3.0.0

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare module 'cloudless-dependencies' {
+    export const Y: any;
+    export const binUtils: any;
+    export const PouchDB: any;
+    export const PouchDBErrors: any;
+    export const extendUsingYArray: Function;
+    export const extendUsingYMap: Function;
+    export const extendUsingYMemory: Function;
+    export const extendUsingYText: Function;
+    export const adapterFun: Function;
+    export const clone: Function;
+    export const EventEmitter: any;
+    export const SocketIO: any;
+}
---
+++ index.js
@@ -0,0 +1,49 @@
+// PouchDB dependencies
+import PouchDB from 'pouchdb-browser';
+import * as PouchDBErrors from 'pouchdb-errors';
+import { adapterFun, clone } from 'pouchdb-utils';
+import {
+    binaryStringToBlobOrBuffer,
+    binaryStringToArrayBuffer,
+    blobOrBufferToBinaryString,
+    blobOrBufferToBase64,
+    base64StringToBlobOrBuffer,
+    readAsBinaryString
+} from 'pouchdb-binary-utils';
+
+// Y-JS dependencies
+import Y from 'yjs';
+import extendUsingYMemory from 'y-memory';
+import extendUsingYMap from 'y-map';
+import extendUsingYArray from 'y-array';
+import extendUsingYText from 'y-text';
+
+// Additional dependencies (ES5)
+import SocketIO from 'socket.io-client';
+import EventEmitter from 'events';
+
+const binUtils = {
+    binaryStringToBlobOrBuffer,
+    readAsBinaryString,
+    binaryStringToArrayBuffer,
+    blobOrBufferToBinaryString,
+    blobOrBufferToBase64,
+    base64StringToBlobOrBuffer
+};
+
+export {
+    binUtils,
+    PouchDB,
+    PouchDBErrors,
+
+    extendUsingYArray,
+    extendUsingYMap,
+    extendUsingYMemory,
+    extendUsingYText,
+    Y,
+
+    adapterFun,
+    clone,
+    EventEmitter,
+    SocketIO
+}
