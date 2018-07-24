71951d374ce7f9c91df4a97184f2c988823c2992
Michel Neumann <neumann.michel93@gmail.com>
2018-07-24 12:29:23 +0200

    Update cloudless-dependencies to v3.1.0

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Version number of package
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+# The commit message for the release
+COMMIT_MSG="Update cloudless-dependencies to v${PACKAGE_VERSION}"
+
+# Staging and committing new files, create version tag and release
+git add lib/ index.* package* gulpfile.js
+git commit -m "$COMMIT_MSG"
+git tag "$PACKAGE_VERSION"
+git push origin master --tags
