5bf91e8320a9769ba94187af56a2c17f992fe71b
Michel Neumann <neumann.michel93@gmail.com>
2018-09-05 14:08:27 +0200

    release 4.0.0

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 // PouchDB dependencies
-import PouchDB from './node_modules/pouchdb-browser/lib/index.es.js';
-import * as PouchDBErrors from './node_modules/pouchdb-errors/lib/index.es.js';
-import { adapterFun } from './node_modules/pouchdb-utils/lib/index.es.js';
+import PouchDB from "pouchdb-browser/lib/index.es.js";
+import * as PouchDBErrors from "pouchdb-errors/lib/index.es.js";
+import { adapterFun } from "pouchdb-utils/lib/index.es.js";
 
 // Y-JS dependencies
-import Y from './node_modules/yjs/y.js';
+import Y from "yjs";
 
 // Additional dependencies (ES5)
-import SocketIO from './node_modules/socket.io-client/lib/index.js';
-import EventEmitter from 'events';
+import SocketIO from "socket.io-client";
+import EventEmitter from "events";
 
-export {
-    adapterFun,
-    PouchDB,
-    PouchDBErrors,
-    Y,
-    EventEmitter,
-    SocketIO
-}
+export { adapterFun, PouchDB, PouchDBErrors, Y, EventEmitter, SocketIO };
